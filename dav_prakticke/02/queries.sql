13d6daf6b93916659fc898c1a97a32707ba650f3
Jan Mazak <jan.mazak@gmail.com>
2023-09-28 12:32:12 +0200

    dav/02

--- dav_prakticke/02/queries.sql
+++ dav_prakticke/02/queries.sql
@@ -10,33 +10,24 @@ FROM employee;
 /* Also, try avoiding ORDER BY and LIMIT to determine minimum. */
 /* For now, we are practicing using WHERE NOT EXISTS on calculating minimum. */
 
-/* 2. Print names and hiring dates of all clerks. */
+/* 2. List names and jobs of employees with salary at least 2000. */
 
-/* 3. List names and jobs of employees with salary at least 2000. */
+/* 3. Print names and salaries of all managers and analysts. */
 
-/* 4. Print names of employees who were hired between 1981 and 1982. */
+/* 4. Print the name of the department in which the president works. (Your query must not use a specific empno.) */
 
-/* 5. Print names and salaries of all managers and analysts. */
+/* 5. Print jobs of employees who work in Chicago. (Your query must not use a specific deptno.) */
 
-/* 6. Print the name of the department in which the president works. (Your query must not use a specific empno.) */
+/* 6. For each employee, print a list of all coworkers (we want the list of all tuples [Employee_name, Location, Coworker]). */
 
-/* 7. Print jobs of employees who work in Chicago. (Your query must not use a specific deptno.) */
+/* 7. Find the lowest salary in New York. */
 
-/* 8. For each employee, print a list of all coworkers, that is, the list all tuples [Employee_name, Location, Coworker]. */
+/* 8. Print names, department names and salaries of all employees whose salary is greater than the lowest salary in department 20. */
 
-/* 9. Print names of employees together with names of their managers. */
+/* 9. Which departments are empty (have no employees)? */
 
-/* 10. Find the lowest salary in New York. */
+/* 10. (1 point) Which departments contain all job positions? (Your query must not rely on specific positions in the given dataset.) */
 
-/* 11. Print names, department names and salaries of all employees whose salary is greater than the lowest salary in department 20. */
+/* 11. (1 point) Which employees manage only clerks? */
 
-/* 12. Which departments contain all job positions? (Your query must not rely on specific positions in the given dataset.) */
-
-/* 13. Which departments are empty (have no employees)? */
-
-/* 14. Which employees manage only clerks? */
-
-/* 15. Which departments employ only salesmen? */
-
-/* 16. Find pairs of departments such that each employee in the first department has higher salary than every employee in the second department. */
-
+/* 12. Which departments employ only salesmen? */
