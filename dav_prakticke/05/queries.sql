dcc999fc39aa27b3e58bd060020135413fdd48e7
Jan Mazak <jan.mazak@gmail.com>
2023-10-19 19:23:49 +0200

    fix: dav c 05

--- dav_prakticke/05/queries.sql
+++ dav_prakticke/05/queries.sql
@@ -6,7 +6,7 @@ Na vypis dna v tyzdni v ludsky citatelnom formate pouzite CASE statement. */
 
 /*  3. Vypiste vsetky trojice [C, K, DatumPozicania], kde K je nazov knihy, ktoru si citatel s menom C pozical, ale este ju nevratil. */
 
-/*  4. Najdite mena citatelov, ktori vratili do 1 mesiaca kazdu z knih, ktore si pozicali. (Vratane citatelov, ktori si nikdy nepozicali nic.) */
+/*  4. Najdite mena citatelov, ktori vratili do 30 dni kazdu z knih, ktore si pozicali. (Vratane citatelov, ktori si nikdy nepozicali nic.) */
 
 /*  5. Vypiste mena citatelov, ktori si pozicali aspon tri rozne knihy, a pritom vsetky ich pozicane knihy boli od toho isteho autora. */
 
@@ -14,7 +14,10 @@ Na vypis dna v tyzdni v ludsky citatelnom formate pouzite CASE statement. */
 
 /*  7. Najdite citatelov, ktori si nepozicali nic pocas prveho roka od registracie. */
 
-/*  8. (1 point) Pre kazdeho citatela vypiste dobu jeho najdlhsej vypozicky (len pre knihy, ktore uz vratil). */
+/*  8. (1 point) Pre kazdeho citatela vypiste dobu jeho najdlhsej ukoncenej vypozicky (pre knihy, ktore uz vratil).
+Pre citatelov, ktori si nic nepozicali, vratte NULL. */
+
+/*  Extra: premyslite si, ako upravit 8 tak, aby sa brali do uvahy aj neukoncene vypozicky. */
 
 /* Data modification */
 
